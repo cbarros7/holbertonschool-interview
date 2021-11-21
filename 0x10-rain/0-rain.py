a08ccfa57843f4b9ab25ea42eed33f632a8b2a82
cbarros7 <cj.barros07@gmail.com>
2021-11-21 18:23:37 -0500

    Fix task

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
-"""
-0_rain.py
-"""
+""" Rain Algorithm """
 
 
 def rain(walls):
+    """Algorithm that calculates water retained
+
+    Args:
+        walls (list): list of walls and their width
+
+    Returns:
+        number: quantity of water retained
     """
-    function that estimates how much water can be retained between
-    single/stacked walls represented in a list of walls
-    """
-    water = 0
-    for i in range(len(walls) - 2):
-        if walls[i] and walls[i + 2]:
-            water += min(walls[i], walls[i + 2])
-    return water
+    if not isinstance(walls, list) and len(walls) < 2:
+        return 0
+
+    if not all(isinstance(n, int) for n in walls):  # any no integer
+        return 0
+
+    rain = 0
+    for i in range(1, len(walls) - 1):
+
+        # Find the maximum element on its left
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        # Find the maximum element on its right
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+
+        # Update the maximum of rain collected
+        rain += (min(left, right) - walls[i])
+
+    return rain
