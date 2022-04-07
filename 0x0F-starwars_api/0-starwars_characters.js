91856257fbd9340942afa99b43ed32af7bbb6b2d
cbarros7 <cj.barros07@gmail.com>
2022-04-06 21:52:45 -0500

    Fix task

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,27 +1,25 @@
 #!/usr/bin/node
 
-const myArray = process.argv.slice(2);
 const request = require('request');
 
-if (Number.isInteger(parseInt(myArray[0]))) {
-  request('https://swapi.dev/api/films/' + myArray[0],
-    async (error, response, body) => {
-      if (error) console.error('error:', error);
-      const linksArray = JSON.parse(body).characters;
-      for (let index = 0; index < linksArray.length; index++) {
-        const name = await getName(linksArray[index]);
-        console.log(name);
-      }
-    });
-}
-
-function getName (link) {
-  const name = new Promise(
-    (resolve, reject) => {
-      request(link, (error, response, body) => {
-        if (error) console.error('error:', error);
-        resolve(JSON.parse(body).name);
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
       });
-    });
-  return name;
-}
+      console.log(res);
+    }
+  }
+});
