80afcd8f3d799330e82f446f8996656cfd8fa226
cbarros7 <cj.barros07@gmail.com>
2022-05-19 21:51:57 -0500

    Add tasks

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void _error(void);
+int _isnumber(char *str);
+int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
